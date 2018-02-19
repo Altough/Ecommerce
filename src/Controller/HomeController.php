ea0ad02e031ec9e787737aa1f44662f39dc51b25
Adama TRAORE <adama.traore@etudiant.univ-reims.fr>
2018-02-19 16:59:11 +0100

    deplacement du form dans produit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -26,16 +26,6 @@ class HomeController extends Controller
             ->getRepository(Category::class)
             ->findAll();
 
-        $form = $this->createForm(ProductForm::class, $products);
-
-        $form->handleRequest($request);
-        //return $this->render('./Product/addAction.html.twig', ['form' => $form->createView(),]);
-
-
-       // return $this->render('Product/show.html.twig',['products' => $products,
-         //   'form' => $form->createView()]);
-
-        return $this->render('./base.html.twig',['products' => $products,
-        'categories' => $categories, 'form' => $form->createView()]);
+        return $this->render('./base.html.twig',['products' => $products, 'categories' => $categories]);
     }
 }
